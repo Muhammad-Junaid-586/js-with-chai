06912cc4bfb0d1bdaf661b611dc26847f963f792
Muhammad Junaid <154722812+Muhammad-Junaid-586@users.noreply.github.com>
2024-05-17 11:10:10 +0000

    configure file done

---
+++ 1_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountName = "Junaid";
+var cityName = "Hangu";
+provinceName = "kpk";
+
+
+// accountId = 456767;
+accountName = "Sami";
+cityName = 'Kohat';
+provinceName = "punjab";
+
+
+console.log(accountId);
+console.table([accountId,cityName,provinceName])
